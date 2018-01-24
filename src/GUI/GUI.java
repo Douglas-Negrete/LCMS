37195b4194095b0802ae1755454b60e65fa55f94
Douglas Negrete <ama824111@gmail.com>
2018-01-23 23:30:23 -0500

    GUI
    Overhauled the GUI to have a better looking menuBar, as well as changing the layout manager to something that made more sense

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -16,6 +16,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -41,48 +42,38 @@ public class GUI extends Application {
 		clients.add(new Client("oso", "7653", 40));
 
 		primaryStage.setTitle("Lawn Care Made Simple");
+        Scene scene = new Scene(new VBox(), 1100, 600);
 
-		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(0, 15, 15, 0));
+		//GridPane grid = new GridPane();
+		//grid.setPadding(new Insets(0, 15, 15, 0));
 
-		MenuBar menuBar = new MenuBar();
+		MenuBar menuBar = new MenuBar();//The menu for the topPane
 		// --- Menu File
-		Menu menuFile = new Menu("File");
+		Menu menuFile = new Menu("File");//file submenu for the menu
 		// --- Menu Edit
-		Menu menuEdit = new Menu("Edit");
+		Menu menuEdit = new Menu("Edit");//edit submenu
 		// --- Menu View
-		Menu menuView = new Menu("View");
-		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
-
-		HBox topPane = new HBox();
-
-		HBox searchBox = new HBox();
-		searchBox.setSpacing(10);
-		searchBox.setPadding(new Insets(5, 20, 5, 20));
-
-		VBox rightPane = new VBox();
-		rightPane.setSpacing(10);
-		rightPane.setPadding(new Insets(20, 20, 20, 20));
-
-		HBox add = new HBox();
-		add.setSpacing(10);
-		add.setPadding(new Insets(20, 20, 20, 20));
-
-		ListView<String> list;
-		ObservableList<String> clientList;
-		TextField searchTextField;
+		Menu menuView = new Menu("View");//temp place holder
+		ObservableList<String> clientList;//the actual string list that will go in the list pane
+		ListView<String> list;//the list pane in the right pane
 		Label searchLabel;
+		TextField searchTextField;
 		Button addClient, addLawn;
+		HBox topPane = new HBox();//what goes in the top section of the layout
+		HBox searchBox = new HBox();//contains the search label, and the search box
+		HBox add = new HBox();//temp for what goes in the center section
+		VBox rightPane = new VBox();//what goes in the right section of the layout
+		BorderPane border = new BorderPane();//the layout for the scene, this layout has five sections: top, left, center, right, bottom
 
-		addClient = new Button("Client");
-		addLawn = new Button("Lawn");
-
-		add.getChildren().addAll(addClient, addLawn);
+		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+		menuBar.setMinWidth(primaryStage.getMaxWidth());
 
 		clientList = FXCollections.<String>observableArrayList(getItem(clients, 0));
+
 		list = new ListView<>(clientList);
 
 		searchLabel = new Label("Search");
+
 		searchTextField = new TextField();
 		searchTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
@@ -99,24 +90,39 @@ public class GUI extends Application {
 
 		});//end setonkeypressed
 
-		searchBox.getChildren().addAll(searchLabel, searchTextField);
+		addClient = new Button("Client");
+
+		addLawn = new Button("Lawn");
 
 		topPane.getChildren().add(menuBar);
 
+		searchBox.setSpacing(10);
+		searchBox.setPadding(new Insets(0, 20, 5, 20));
+		searchBox.getChildren().addAll(searchLabel, searchTextField);
+
+		add.setSpacing(10);
+		add.setPadding(new Insets(20, 20, 20, 20));
+
+		add.getChildren().addAll(addClient, addLawn);
+
+		rightPane.setSpacing(10);
+		rightPane.setPadding(new Insets(20, 20, 20, 20));
 		rightPane.getChildren().addAll(searchBox, list);
+
+		border.setTop(topPane);
+		border.setCenter(add);
+		border.setRight(rightPane);
 
 		//		grid.add(searchLabel, 0, 0);
 		//		grid.add(searchTextField, 1, 0);
-		grid.add(topPane, 0, 0);
-		grid.add(add, 0, 1);
-		grid.add(rightPane, 1, 0);
+
 		//grid.add(searchBox, 1, 0);
 		//grid.add(list, 1, 2);
 
-		primaryStage.setScene(new Scene(grid, 1100, 600));
-
+		//primaryStage.setScene(new Scene(border, 1100, 600));
+		((VBox) scene.getRoot()).getChildren().addAll(menuBar, border);
+		primaryStage.setScene(scene);
 		primaryStage.show();
-
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 
 			public void handle(WindowEvent we) {
@@ -136,7 +142,7 @@ public class GUI extends Application {
 			System.out.println("Is contained in the list.");
 
 		}
-		
+
 	}//end search
 
 	public String[] getItem(ArrayList<Client> list, int index) {
